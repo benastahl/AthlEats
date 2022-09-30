89d5dbf0ef85706491db5a946a747e4e9a4fbea3
Matthew-A-Watkins <mattheww@tkins.me>
2022-09-30 11:35:03 -0400

    authority

--- account_authority.py
+++ account_authority.py
@@ -12,21 +12,6 @@ class Administrator:
         self.first_name = email.split('@')[0].split('_')[0]
         self.last_name = email.split('@')[0].split('_')[1]
 
-    def update_password(self, password):
-        self.password = password
-
-    def update_grade(self, grade):
-        self.grade = grade
-
-    def get_email(self):
-        return self.email
-
-    def get_password(self):
-        return self.password
-
-    def get_grade(self):
-        return self.password
-
 
 class User:
     def __init__(self, email, password, grade, payment_type):
@@ -41,25 +26,4 @@ class User:
         self.email = email
         self.first_name = email.split('@')[0].split('_')[0]
         self.last_name = email.split('@')[0].split('_')[1]
-
-    def update_password(self, password):
-        self.password = password
-
-    def update_grade(self, grade):
-        self.grade = grade
-
-    def update_payment_type(self, payment_type):
-        self.payment_type = payment_type
-
-    def get_email(self):
-        return self.email
-
-    def get_password(self):
-        return self.password
-
-    def get_grade(self):
-        return self.password
-
-    def get_payment_type(self):
-        return self.payment_type
 
