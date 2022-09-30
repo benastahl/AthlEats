c48c1d118e92d9071d72007be0144ec2c5d93f93
Matthew-A-Watkins <mattheww@tkins.me>
2022-09-30 09:28:42 -0400

    requirements

--- account_authority.py
+++ account_authority.py
@@ -4,8 +4,28 @@ class Administrator:
         self.email = email
         self.password = password
         self.grade = grade
-        first_name = email.split('@')[0].split('_')[0]
-        last_name = email.split('@')[0].split('_')[1]
+        self.first_name = email.split('@')[0].split('_')[0]
+        self.last_name = email.split('@')[0].split('_')[1]
+
+    def update_email(self, email):
+        self.email = email
+        self.first_name = email.split('@')[0].split('_')[0]
+        self.last_name = email.split('@')[0].split('_')[1]
+
+    def update_password(self, password):
+        self.password = password
+
+    def update_grade(self, grade):
+        self.grade = grade
+
+    def get_email(self):
+        return self.email
+
+    def get_password(self):
+        return self.password
+
+    def get_grade(self):
+        return self.password
 
 
 class User:
@@ -14,5 +34,32 @@ class User:
         self.password = password
         self.grade = grade
         self. payment_type = payment_type
-        first_name = email.split('@')[0].split('_')[0]
-        last_name = email.split('@')[0].split('_')[1]
+        self.first_name = email.split('@')[0].split('_')[0]
+        self.last_name = email.split('@')[0].split('_')[1]
+
+    def update_email(self, email):
+        self.email = email
+        self.first_name = email.split('@')[0].split('_')[0]
+        self.last_name = email.split('@')[0].split('_')[1]
+
+    def update_password(self, password):
+        self.password = password
+
+    def update_grade(self, grade):
+        self.grade = grade
+
+    def update_payment_type(self, payment_type):
+        self.payment_type = payment_type
+
+    def get_email(self):
+        return self.email
+
+    def get_password(self):
+        return self.password
+
+    def get_grade(self):
+        return self.password
+
+    def get_payment_type(self):
+        return self.payment_type
+
