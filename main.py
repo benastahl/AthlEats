c157220789fe954113625ba7fd50e1af7b9ba6d8
Ben Stahl <benastahl@gmail.com>
2022-10-17 09:35:03 -0400

    updated

--- main.py
+++ main.py
@@ -16,58 +16,83 @@ def home():
 
     if auth_token:
         # Checks to see if there's a corresponding user with auth token.
-        if UserDB().get_user(auth_token=auth_token):
-            return redirect("/dashboard", 302)
+        user = UserDB().get_user(auth_token=auth_token)
+        return render_template("home.html", user=False)
 
     # Redirects back to log in if no auth token found
-    return redirect("/login", 302)
+    return render_template("home.html", user=False)
 
 
-@app.route("/dashboard", methods=["GET"])
-def display_dashboard():
-
-    # # Redirects to dashboard if user has auth_token cookie (otherwise redirects to signup)
-    # auth_token = request.cookies.get("auth_token")
-    #
-    # if not auth_token:
-    #     # Redirects back to log in if no auth token found
-    #     return redirect("/login", 302)
-    #
-    # # Checks to see if there's a corresponding user with auth token.
-    # user = UserDB().get_user(auth_token=auth_token)
-    # if not user:
-    #     # Redirects back to log in if user not found
-    #     return redirect("/login", 302)
-
-    return render_template("dashboard.html")
-
-
-@app.route("/login", methods=["GET"])
-def display_login():
-    return render_template("login.html")
-
-
-@app.route("/login", methods=["POST"])
-def process_login():
-    email = request.form["email"]
-    password = request.form["pass"]  # plain text password
+def process_login(_request):
+    email = _request.form["email"]
+    password = _request.form["pass"]  # plain text password
 
     # login_user returns auth_token of user if email and password correct
     auth_token = UserDB().login_user(email, password)
     if not auth_token:
-        return render_template("login.html", message="Password or email incorrect not found")
+        return render_template("home.html", login_message="Password or email incorrect not found")
 
-    response = redirect("/dashboard", 302)
+    response = redirect("/", 302)
     # Create an auth browser cookie (random letters and numbers) as our authentication
     # token so the user doesn't have to log in every single time.
     response.set_cookie('auth_token', auth_token, max_age=31540000)  # One year expiration (in seconds)
+    return response
+
+
+def process_signup(_request):
+    # Collect POST request params from signup
+    tos_agree = _request.form.get("tos-agree")
+    email = _request.form["email"]
+    grade = int(_request.form["grade"])
+    # Hash password (hashing means that it is encrypted and impossible to decrypt)
+    # We can now only check to see if a plain text input matches the hashed password (bcrypt.checkpw).
+    hashed_password = bcrypt.hashpw(bytes(str(_request.form["pass"]).encode("utf-8")), bcrypt.gensalt()).decode("utf-8")
+
+    # Check if email is already in use (get_student returns list of users with that email).
+    if UserDB().get_user(email=email):
+        return render_template("home.html", signup_message="Email is already in use.")
+
+    # Check if email is valid student email
+    if "@student.waylandps.org" not in email:
+        return render_template("home.html", signup_message="Not a valid WHS student email.")
+
+    # Checks if email is a real valid email address
+    # if not validate_email(email, verify=True):
+    #     return render_template("signup.html", message="Not a real email address.")
+
+    # Check to see if Terms of Service is agreed to
+    if tos_agree != "on":
+        return render_template("home.html", signup_message="Please agree to TOS to access the website.")
+
+    # Set auth cookie token
+    response = redirect("/", 302)
+
+    # Create an auth browser cookie (random letters and numbers) as our authentication
+    # token so the user doesn't have to log in every single time.
+    auth_token = secrets.token_hex()
+    response.set_cookie('auth_token', auth_token, max_age=31540000)  # One year expiration (in seconds)
+
+    # Parse first and last name from email address.
+    first_name = email.split("_")[0].capitalize()
+    last_name = email.split("_")[1].split("@")[0].capitalize()
+
+    # Create timestamp of the creation date of the account
+    creation_date = int(datetime.timestamp(datetime.now()))
+
+    # Add user to database
+    UserDB().add_user(first_name=first_name, last_name=last_name, email=email, grade=grade,
+                      hashed_password=hashed_password, auth_token=auth_token, creation_date=creation_date)
 
     return response
 
 
-@app.route("/signup", methods=["GET"])
-def display_signup():
-    return render_template("signup.html")
+@app.route("/", methods=["POST"])
+def process_homepage():
+    if "tos" in request.form:
+        return process_signup(_request=request)
+    else:
+        return process_login(_request=request)
+
 
 
 @app.route("/admin-login", methods=["GET"])
@@ -92,55 +117,7 @@ def process_admin_login():
 
 @app.route("/admin-dashboard", methods=["GET"])
 def display_admin_dashboard():
-    return render_template("admin-dashboard.html")
-
-
-@app.route("/signup", methods=["POST"])
-def process_signup():
-    # Collect POST request params from signup
-    tos_agree = request.form.get("tos-agree")
-    email = request.form["email"]
-    grade = int(request.form["grade"])
-    # Hash password (hashing means that it is encrypted and impossible to decrypt)
-    # We can now only check to see if a plain text input matches the hashed password (bcrypt.checkpw).
-    hashed_password = bcrypt.hashpw(bytes(str(request.form["pass"]).encode("utf-8")), bcrypt.gensalt()).decode("utf-8")
-
-    # Check if email is already in use (get_student returns list of users with that email).
-    if UserDB().get_user(email=email):
-        return render_template("signup.html", message="Email is already in use.")
-
-    # Check if email is valid student email
-    if "@student.waylandps.org" not in email:
-        return render_template("signup.html", message="Not a valid WHS student email.")
-
-    # Checks if email is a real valid email address
-    # if not validate_email(email, verify=True):
-    #     return render_template("signup.html", message="Not a real email address.")
-
-    # Check to see if Terms of Service is agreed to
-    if tos_agree != "on":
-        return render_template("signup.html", message="Please agree to TOS to access the website.")
-
-    # Set auth cookie token
-    response = redirect("/dashboard", 302)
-
-    # Create an auth browser cookie (random letters and numbers) as our authentication
-    # token so the user doesn't have to log in every single time.
-    auth_token = secrets.token_hex()
-    response.set_cookie('auth_token', auth_token, max_age=31540000)  # One year expiration (in seconds)
-
-    # Parse first and last name from email address.
-    first_name = email.split("_")[0].capitalize()
-    last_name = email.split("_")[1].split("@")[0].capitalize()
-
-    # Create timestamp of the creation date of the account
-    creation_date = int(datetime.timestamp(datetime.now()))
-
-    # Add user to database
-    UserDB().add_user(first_name=first_name, last_name=last_name, email=email, grade=grade,
-                      hashed_password=hashed_password, auth_token=auth_token, creation_date=creation_date)
-
-    return response
+    return render_template("admin-home.html")
 
 
 if __name__ == '__main__':
