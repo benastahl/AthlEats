624a5fb40c69050b23a27d52f9b84ad65b372031
Ben Stahl <benastahl@gmail.com>
2022-11-02 10:16:16 -0400

    zocco fuck me

--- zocco_request_template.py
+++ zocco_request_template.py
@@ -4,6 +4,7 @@ s = requests.session()
 def get_data():
     request_url = "http://httpbin.org/get"
     get_request = s.get(request_url)
+    print(get_request)
 
 if __name__ == '__main__':
     get_data()
