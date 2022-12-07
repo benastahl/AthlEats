b0f168a30661899dc5e7740bb7380fb479db0714
Ben Stahl <benastahl@gmail.com>
2022-12-07 11:10:22 -0500

    coconutmalld reverse

--- app.py
+++ app.py
@@ -52,9 +52,9 @@ for handle in error_handles.items():
     )
 
 
-@app.before_request
-def coconut_malld():
-    return render_template("coconut.html")
+# @app.before_request
+# def coconut_malld():
+#     return render_template("coconut.html")
 
 
 @app.route("/", methods=["GET"])
