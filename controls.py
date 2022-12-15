c3ae298014a16cb1e4e81bc6e44ad4add56fa1e5
Matthew-A-Watkins <mattheww@tkins.me>
2022-12-15 14:17:44 -0500

    Secrets hahahah

--- controls.py
+++ controls.py
@@ -9,9 +9,14 @@ from datetime import datetime
 from termcolor import colored
 from account_authority import User, Order
 
+
 sql_username = loads(Path("SECRET_TOKEN.json").read_text())["sql_username"]
 sql_password = loads(Path("SECRET_TOKEN.json").read_text())["sql_password"]
 sql_host = loads(Path("SECRET_TOKEN.json").read_text())["sql_host"]
+
+google_password = loads(Path("SECRET_TOKEN.json").read_text())["google_password"]
+google_username = loads(Path("SECRET_TOKEN.json").read_text())["google_username"]
+drive_api_key = loads(Path("SECRET_TOKEN.json").read_text())["drive_api_key"]
 
 # dialect+driver://username:password@host:port/database
 
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from json import loads
 from pathlib import Path
 
-
-
 sql_username = loads(Path("SECRET_TOKEN.json").read_text())["sql_username"]
 sql_password = loads(Path("SECRET_TOKEN.json").read_text())["sql_password"]
 sql_host = loads(Path("SECRET_TOKEN.json").read_text())["sql_host"]
 
-print(sql_username, sql_password, sql_host)
+google_password = loads(Path("SECRET_TOKEN.json").read_text())["google_password"]
+google_username = loads(Path("SECRET_TOKEN.json").read_text())["google_username"]
+drive_api_key = loads(Path("SECRET_TOKEN.json").read_text())["drive_api_key"]
+
+
+print(sql_username, sql_password, sql_host, google_username, google_password)
