5b5c482b42865fd4e84acbf3d202de5896e37e78
Matthew Watkins <mattheww@tkins.me>
2023-01-04 03:03:10 -0500

    Google Drive API integration setup

--- image_processing.py
+++ image_processing.py
@@ -1,33 +1,136 @@
-from pydrive.drive import GoogleDrive
-from pydrive.auth import GoogleAuth
+from __future__ import print_function
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
+from googleapiclient.http import MediaFileUpload
 
-# For using listdir()
-import os
+def get_service(api_name, api_version, scopes, key_file_location):
+    """Get a service that communicates to a Google API.
 
-# Below code does the authentication
-# part of the code
-gauth = GoogleAuth()
+    Args:
+        api_name: The name of the api to connect to.
+        api_version: The api version to connect to.
+        scopes: A list auth scopes to authorize for the application.
+        key_file_location: The path to a valid service account JSON key file.
 
-# Creates local.db webserver and auto
-# handles authentication.
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
+    Returns:
+        A service that is connected to the specified API.
+    """
 
-# replace the value of this variable
-# with the absolute path of the directory
-path = r"C:\Games\Battlefield"
+    credentials = service_account.Credentials.from_service_account_file(
+    key_file_location)
 
-# iterating thought all the files/folder
-# of the desired directory
-for x in os.listdir(path):
-    f = drive.CreateFile({'title': x})
-    f.SetContentFile(os.path.join(path, x))
-    f.Upload()
+    scoped_credentials = credentials.with_scopes(scopes)
 
-    # Due to a known bug in pydrive if we
-    # don't empty the variable used to
-    # upload the files to Google Drive the
-    # file stays open in memory and causes a
-    # memory leak, therefore preventing its
-    # deletion
-    f = None
+    # Build the service object.
+    service = build(api_name, api_version, credentials=scoped_credentials)
+
+    return service
+
+def upload(file_path):
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+
+    # Define the auth scopes to request.
+    scope = 'https://www.googleapis.com/auth/drive'
+    key_file_location = 'receipt-storage-372419-0812a91ef949.json'
+
+    try:
+
+        file_metadata = {
+            # name of file to be uploaded
+            'name': file_path,
+
+            # folder to which file is uploaded
+            # I'm too lazy to make it dynamic so if you share it a new folder
+            # list the files and choose change this to that folder id
+            'parents': ['13Wgl6RYFvZaWuErS8LXY-LzkDeAbQ6GA']
+                         }
+
+        media = MediaFileUpload(file_path, mimetype='image/png', resumable=True)
+
+
+        # Authenticate and construct service.
+        service = get_service(
+            api_name='drive',
+            api_version='v3',
+            scopes=[scope],
+            key_file_location=key_file_location)
+
+        # Call the Drive v3 API
+        service.files().create(body=file_metadata, media_body=media).execute()
+
+        list_files(key_file_location)
+
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+
+# Use only to clear files in root that cannot be manually deleted elsewhere
+def purge(key_file_location, scope = 'https://www.googleapis.com/auth/drive'):
+
+    try:
+        # Authenticate and construct service.
+        service = get_service(
+            api_name='drive',
+            api_version='v3',
+            scopes=[scope],
+            key_file_location=key_file_location)
+
+        # Call the Drive v3 API
+        results = service.files().list(
+            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+
+        items = results.get('files', [])
+
+        if not items:
+            print('No files found.')
+            return
+
+        print('==deleting files==:')
+        for item in items:
+            try:
+                service.files().delete(fileId=item['id']).execute()
+                print("{} ({}) deleted successfully".format(item['name'], item['id']))
+            except:
+                print("Could not delete file: {} ({})".format(item['name'], item['id']))
+
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+
+
+def list_files(key_file_location):
+    """Shows basic usage of the Drive v3 API.
+        Prints the names and ids of the first 10 files the user has access to.
+        """
+
+    # Define the auth scopes to request.
+    scope = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+
+    try:
+        # Authenticate and construct service.
+        service = get_service(
+            api_name='drive',
+            api_version='v3',
+            scopes=[scope],
+            key_file_location=key_file_location)
+
+        # Call the Drive v3 API
+        results = service.files().list(
+            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+
+        if not items:
+            print('No files found.')
+            return
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+
+# if __name__ == '__main__':
+    # this will delete everything, use at your own risk
+    # purge(key_file_location='receipt-storage-372419-0812a91ef949.json')
+    # list_files(key_file_location='receipt-storage-372419-0812a91ef949.json')
